9560279fd2ab1d4f938ddae31075ee9278e19da3
deemacauses <deemacauses@outlook.com>
2023-03-19 09:58:18 +0200

    New: Enhance through adding user migration

---
+++ src/db/migrations/20230319074031-create-user.js
@@ -0,0 +1,61 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "user",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+      createdBy: {
+        type: Sequelize.INTEGER,
+      },
+      updatedBy: {
+        type: Sequelize.INTEGER,
+      },
+      deletedBy: {
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("user");
+  },
+};
